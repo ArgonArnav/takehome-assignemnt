aed17f00d9d3ca62079e845c4969aeb825307858
Arnav Anand <anandarnav2016@gmail.com>
2024-04-22 13:56:14 -0500

    main file host added

--- main.py
+++ main.py
@@ -125,4 +125,4 @@ def process():
     return render_template('result.html', extracted_facts=extracted_facts)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
